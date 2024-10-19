c210739d429be48fcce11bac0775f67e7a6c21d2
Matheuskocotem <Kocotemmatheus@gmail.com>
2024-10-19 01:29:13 -0300

    Provider

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -9,9 +9,16 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+        // Bind de repositórios
+        $this->app->bind(RoomRepository::class, function ($app) {
+            return new RoomRepository($app->make('App\Models\Room'));
+        });
+
+        $this->app->bind(MeetingRepository::class, function ($app) {
+            return new MeetingRepository($app->make('App\Models\Meeting'));
+        });
     }
 
     /**
