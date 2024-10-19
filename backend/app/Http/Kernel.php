86ae41acd5631d42cd19de89882df05e0f117e27
Matheuskocotem <Kocotemmatheus@gmail.com>
2024-10-19 14:59:34 -0300

    Auth

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -68,6 +68,6 @@ class Kernel extends HttpKernel
 
     protected $routeMiddleware = [
         // outros middlewares...
-        'isAdmin' => \App\Http\Middleware\IsAdmin::class,
+        'role' => \App\Http\Middleware\CheckRole::class,
     ];
 }
