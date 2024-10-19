daeed39b96fa7468bda9d936dd617c717a3025a7
Matheuskocotem <Kocotemmatheus@gmail.com>
2024-10-19 15:01:31 -0300

    routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,18 +2,35 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MeetingRoomController;
+use App\Http\Controllers\MeetingController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    // Public routes
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    // Protected routes
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::put('/update/{id}', [AuthController::class, 'update']);
+        Route::delete('/delete/{id}', [AuthController::class, 'delete']);
+
+        // Meeting Room routes with 'api/rooms' prefix
+        Route::prefix('rooms')->group(function () {
+            Route::get('/', [MeetingRoomController::class, 'index']);
+            Route::post('/', [MeetingRoomController::class, 'store'])->middleware('role:admin');
+            Route::get('/{id}', [MeetingRoomController::class, 'show']);
+            Route::put('/{id}', [MeetingRoomController::class, 'update'])->middleware('role:admin');
+            Route::delete('/{id}', [MeetingRoomController::class, 'destroy'])->middleware('role:admin');
+        });
+
+        Route::prefix('meetings')->group(function () {
+            Route::get('/', [MeetingController::class, 'index']);
+            Route::post('/', [MeetingController::class, 'store']);
+            Route::get('/{id}', [MeetingController::class, 'show']);
+            Route::put('/{id}', [MeetingController::class, 'update']);
+            Route::delete('/{id}', [MeetingController::class, 'destroy']);
+        });
+    });
